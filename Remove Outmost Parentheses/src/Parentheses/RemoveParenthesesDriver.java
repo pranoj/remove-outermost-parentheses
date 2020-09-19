cfd7db7febac8836e1fe0240723290795cb0981f
prano <pranoj12@gmail.com>
2020-09-19 09:33:54 -0500

    edited the class RemoveParenthesesDriver

--- Remove Outmost Parentheses/src/Parentheses/RemoveParenthesesDriver.java
+++ Remove Outmost Parentheses/src/Parentheses/RemoveParenthesesDriver.java
@@ -6,6 +6,18 @@ public class RemoveParenthesesDriver
 	{
 		String S = "(()())(())";
 		System.out.println("Let's remove outermost parentheses from: " + S);
-		System.out.print(removeOutermostParentheses);
+		System.out.print(removeOutermostParentheses(S));
 	} // end main
-}
+
+    public static String removeOutermostParentheses(String S) 
+    {
+		StringBuilder s = new StringBuilder();
+		int opened = 0;
+		for (char c : S.toCharArray()) 
+		{
+		    if (c == '(' && opened++ > 0) s.append(c);
+		    if (c == ')' && opened-- > 1) s.append(c);
+		}
+        return s.toString();
+    } // end removeOutermostParentheses
+} // end class RemoveParentheseDriver
